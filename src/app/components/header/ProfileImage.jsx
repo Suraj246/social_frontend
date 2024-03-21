916ec4481df1b4cbfc2e57ff36242abf510d99ff
suraj <surajsurwase9881@gmail.com>
2024-03-21 09:36:09 +0530

    image background color changed again

--- src/app/components/header/ProfileImage.jsx
+++ src/app/components/header/ProfileImage.jsx
@@ -6,15 +6,15 @@ const ProfileImage = ({ userProfileData, imagePreview }) => {
     return (
         <div className=' w-full h-full'>
             <Image
-                src={imagePreview ? `${imagePreview}` : `${API}/uploads/${userProfileData?.data?.image} `}
-                // src={imagePreview ? `${imagePreview}` : userProfileData?.data?.image ? `${API}/uploads/${userProfileData?.data?.image} ` : ""}
+                src={imagePreview ? imagePreview : `${API}/uploads/${userProfileData?.data?.image}`}
                 alt="Profile Preview"
                 className="w-full h-full rounded-full object-cover"
                 width={500} height={500}
-                style={{ backgroundColor: userProfileData?.data?.image ? "black" : "black" }}
+                style={{ backgroundColor: imagePreview ? "black" : "white" }} // Adjusted style based on image availability
             />
         </div>
     )
 }
+// src={imagePreview ? `${imagePreview}` : userProfileData?.data?.image ? `${API}/uploads/${userProfileData?.data?.image} ` : ""}
 
 export default ProfileImage
